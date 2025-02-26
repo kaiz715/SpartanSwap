a31726c7b5408d01439f4d41f494187ab2482bf5
williserdman <willis.erdman@gmail.com>
2025-02-26 11:52:27 -0500

    fixed eslint error, changed base path

--- spartanswap/app/page.tsx
+++ spartanswap/app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
 import HomePage from "./components/homepage";
 
+export default function Page() {
+    return <HomePage />;
+}
+
+// page.tsx
+// use CSR
+/* import { useEffect, useState } from "react";
 
 export default function Page() {
-  return <HomePage />;
-}
+    const [isServer, setIsClient] = useState(true);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isServer) return null;
+
+    return <HomePage />;
+} */
