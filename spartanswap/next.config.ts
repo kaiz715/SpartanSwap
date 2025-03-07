235fbb5222d4aefaea2887562f05a5226edc0e29
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2025-03-07 14:46:12 -0500

    fix local

--- spartanswap/next.config.ts
+++ spartanswap/next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
      */
     output: "export",
 
-<<<<<<< HEAD
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
@@ -26,25 +25,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-=======
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "", //"/SpartanSwap",
-
-    assetPrefix: "", //"/SpartanSwap/",
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-        unoptimized: true,
-    },
->>>>>>> ef6da181063347ff898cd802e0f361bd488b49e4
 
     env: {
         NEXT_PUBLIC_BASE_PATH: ``, //`/SpartanSwap`,
