764d5e20af176fcf41c4ed06589ac853d0cbca80
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2025-04-10 16:39:32 -0400

    fixing my server

---
+++ spartanswap/globals.d.ts
@@ -0,0 +1,7 @@
+// globals.d.ts
+declare module "next/link";
+declare module "next/image";
+declare module "next/navigation";
+declare module "framer-motion";
+declare module "react-cookie";
+declare module "axios";
