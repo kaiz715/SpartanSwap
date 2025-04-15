27c6f473649f75eef111d58d5fd934b2575f25ec
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2025-04-14 20:35:05 -0400

    Disable ESLint errors during build

--- spartanswap/next.config.ts
+++ spartanswap/next.config.ts
@@ -1,40 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
-    /**
-     * Enable static exports.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  //basePath: "/SpartanSwap",
-
-  //assetPrefix: "/SpartanSwap/",
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
-
-    env: {
-        NEXT_PUBLIC_BASE_PATH: ``, //`/SpartanSwap`,
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
